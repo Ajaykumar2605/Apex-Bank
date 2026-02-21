25ddf3f60c8bba7790dfb1a61bc724d2c6a343c5
M.Ajay Kumar <144547135+Ajaykumar2605@users.noreply.github.com>
2026-02-21 12:05:45 +0530

    Update transfer.php for database connection and layout

--- transfer.php
+++ transfer.php
@@ -36,8 +36,8 @@
   </div>
 </nav>
 <?php
-            $connection = mysqli_connect("127.0.0.1","apex_user","StrongPassword123!","apexbank_db");
-$db = mysqli_select_db($connection,"apexbank_db");
+            $connection = mysqli_connect("192.168.1.20","apex_user","redhat","apexbank_db");
+$db = mysqli_select_db($connection,"apexbank_db")
             
              $sql = "select * from Account";
              $run = mysqli_query($connection, $sql);
@@ -239,3 +239,4 @@ $db = mysqli_select_db($connection,"apexbank_db");
 </body>
 
 </html>
+
